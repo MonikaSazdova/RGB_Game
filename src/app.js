7a9b1e2ba5a2be23cc5395edc7a9a713a1edb023
Monika Sazdova <59760777+MonikaSazdova@users.noreply.github.com>
2020-11-17 17:26:57 +0100

    Update app.js
    Added random color generator

--- src/app.js
+++ src/app.js
@@ -1,16 +1,10 @@
-const colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)"
-]
+const colors = generateRandomColors(6);
 
 let squares = document.querySelectorAll(".square");
 let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
 let messageDisplay = document.querySelector("#message");
+let h1 = document.querySelector("h1");
 
 //add picked collor to the title display
 colorDisplay.textContent = pickedColor;
@@ -28,6 +22,8 @@ for(let i = 0; i < squares.length; i++){
 			if(clickedColor === pickedColor){
 				messageDisplay.textContent = "Correct!";
 				changeColors(clickedColor);
+				h1.style.backgroundColor = clickedColor;
+
 			} else {
 				this.style.backgroundColor = "#232323";
 				messageDisplay.textContent = "Try Again";
@@ -50,3 +46,27 @@ function pickColor(){
 	let random = Math.floor(Math.random() * colors.length);
 	return colors[random]
 }
+
+//generate random colors
+function generateRandomColors(num){
+	//make an array
+	let arr = [];
+	//add num random colors to array
+	for(let i = 0; i < num; i++){
+		//getRandom color and push into array
+		arr.push(randomColor())
+	}
+	//return the array
+	return arr;
+}
+
+function randomColor(){
+	//pick a "red" from 0 to 255
+	let r = Math.floor(Math.random() * 256);
+	//pick a "green" from 0 to 255
+	let g = Math.floor(Math.random() * 256);
+	//pick a "blue" from 0 to 255
+	let b = Math.floor(Math.random() * 256);
+	return `rgb(${r}, ${g}, ${b})`
+}
+
