bb1df02104b383cdce58d7d0b59d8ad86cb27df6
Monika Sazdova <59760777+MonikaSazdova@users.noreply.github.com>
2020-11-17 22:44:28 +0100

    Added styling

--- src/app.js
+++ src/app.js
@@ -51,7 +51,7 @@ resetBtn.addEventListener("click", function(){
 	for(let i = 0; i < squares.length; i++) {
 		squares[i].style.backgroundColor = colors[i];
 	}
-	h1.style.backgroundColor = "#232323"
+	h1.style.backgroundColor = "steelblue"
 })
 
 
